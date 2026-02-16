d67a563bd62a4723a1eb0f1d69bc8768f613d139
Saadbinzahid1 <sbinzahid2@gmail.com>
2026-02-16 16:06:07 +0500

    5th Project done

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,10 @@ module.exports = {
   darkMode: "class",
   content: ["./*.html", "./**/*.html"], // This looks at the root and all subfolders
   theme: {
+    fontfamily: {
+      sans: ["Mulish", "sans-serif"],
+      mono: ["Rokkitt", "monospace"],
+    },
     extend: {},
   },
   plugins: [],
